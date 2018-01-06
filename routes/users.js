70ee0cf876722c67c1b7e90efd790d5b42cd02fa
dangelsc <dangel_sc@hotmail.com>
2018-01-05 23:32:27 -0400

    por fin

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/nuevo', function(req, res, next) {
+  res.send('este es una nueva ruta1');
+});
 
+router.get('/nuevo2', function(req, res, next) {
+  res.send('este es una nueva ruta11');
+});
+
+router.get('/nuevo3', function(req, res, next) {
+  res.send('este es una nueva ruta111');
+});
 module.exports = router;
